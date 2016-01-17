da982d996f931f4eedfee0e0cd509efd0633e5b4
vitorlui <vitorlui@gmail.com>
2016-01-17 12:44:36 +0100

    SOAP Web Services Working Right =D

--- BlaBlaLanguageWeb/src/swSoap/UpdateEvent.java
+++ BlaBlaLanguageWeb/src/swSoap/UpdateEvent.java
@@ -1,9 +1,16 @@
 package swSoap;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import Models.Event;
 import Models.User;
+import Utils.CrudEvent;
 import Utils.CrudUser;
 
 @WebService
@@ -14,6 +21,29 @@ public class UpdateEvent {
 	public String UpdateEvent(int eventId, int Establishment, int Language, String Name, String DateEvent,String Description){		
 		String ret="";
 		try{
+			
+		CrudEvent ce = new CrudEvent();
+			
+			Event e = new Event();
+			
+			e.setId(eventId);
+			e.setEstabId(Establishment);
+			e.setLangId(Language);
+			e.setName(Name);	
+			e.setDescription(Description);
+			
+			//String string_date = "14/01/2016 12:10:00";
+			String string_date = DateEvent.toString().trim();
+
+			SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+			Date d = f.parse(string_date);
+			long milliseconds = d.getTime();
+			
+			e.setTim(String.valueOf(milliseconds));	
+			
+			if(!ce.updateEvent(e, null)) throw new Exception("Error in update event, try to verify the id ranges");
+		
+	     	//ret = "Sucess";
 			ret = "Sucess";
 			
 		}catch(Exception ex){			
